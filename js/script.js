e6a2b6208a687981acc0b1fd40f09c2cc71612d1
Su4r3z <vitoriosuarez.06@gmail.com>
2023-10-10 22:30:23 -0300

    att Js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 function mostrarInput(id) {
     let esfera = document.querySelector("#" + id).previousElementSibling;
     let input = document.getElementById(id);
-    let esfera2 = document.getElementById('#input2');
 
     if (id === 'input2') {
         esfera.style.transform = "translateX(-10px)";
       } else {
-        // Move as outras esferas para a esquerda
         esfera.style.transform = "translateX(-10px)";
       }
     // Torna o input visível
